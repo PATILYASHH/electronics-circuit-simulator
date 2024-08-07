8702e8dbb3b654fa1d5bee728039c720d8ae406c
Zakronindustry <pranayjain1001@gmail.com>
2024-08-07 10:41:53 +0530

    Replace old code with my React project

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <div className="flex flex-col border-2 border-black h-screen w-[10%] bg-black text-white">
+       <div className="flex rounded-t-lg border-2 justify-center border-write  h-[10%] w-[90%] mt-2 ml-1 mr-1 hover:bg-white hover:text-black">
+        choose
+       </div>
+       <div className="flex border-2 border-write justify-center h-[10%] w-[90%] ml-1 mr-1 hover:bg-white hover:text-black">
+        Gates
+       </div>
+       <div className="flex border-2 border-write justify-center h-[10%] w-[90%] ml-1 mr-1 hover:bg-white hover:text-black">
+        Resistace
+       </div>
+       <div className="flex border-2 border-write justify-center h-[10%] w-[90%] ml-1 mr-1 hover:bg-white hover:text-black">
+        Device
+       </div>
+       <div className="flex border-2 border-write justify-center h-[10%] w-[90%] ml-1 mr-1 hover:bg-white hover:text-black">
+        Wire
+       </div>
+       <div className="flex border-2 border-write justify-center h-[10%] w-[90%] ml-1 mr-1 hover:bg-white hover:text-black">
+        Electrifiy
+       </div>
+       <div className="flex border-2 border-write justify-center h-[10%] w-[90%] ml-1 mr-1 hover:bg-white hover:text-black">
+        Conductor
+       </div>
+       <div className="flex border-2 border-write justify-center h-[10%] w-[90%] ml-1 mr-1 hover:bg-white hover:text-black">
+        Power Control
+       </div>
+       <div className="flex border-2 border-write rounded-b-lg justify-center h-[10%] w-[90%] ml-1 mr-1 hover:bg-white hover:text-black">
+          
+       </div>
+      </div>
+      <div className="flex justify-start flex-col  border-2 border-black bg-black h-screen w-full">
+        <div className="flex h-[5%] flex-row w-full  ">
+         <div className="flex border-2 h-full w-[10%] justify-center bg-black text-white border-white rounded-l-lg mt-1 mb-1 hover:bg-white hover:text-black">
+          a
+         </div>
+         <div className="flex border-2 justify-center bg-black text-white border-white h-full w-[10%] mt-1 mb-1 hover:bg-white hover:text-black">
+          b
+         </div>
+         <div className="flex border-2 justify-center bg-black text-white border-white h-full w-[10%]  mt-1 mb-1 hover:bg-white hover:text-black">
+          c
+         </div>
+         <div className="flex border-2 justify-center bg-black text-white border-white h-full w-[10%]  mt-1 mb-1 hover:bg-white hover:text-black">
+          d
+         </div>
+         <div className="flex border-2 justify-center bg-black text-white border-white h-full w-[10%]  mt-1 mb-1 hover:bg-white hover:text-black">
+          e
+         </div>
+         <div className="flex border-2 justify-center bg-black text-white border-white h-full w-[10%] rounded-r-lg mt-1 mb-1 hover:bg-white hover:text-black">
+          f
+         </div>
+         <div className="flex border-2 justify-center bg-black text-white border-white ml-4 h-full w-[10%] rounded-lg mt-1 mb-1 hover:bg-white hover:text-black">
+          Tools
+         </div>
+         <div className="flex border-2 justify-center text-white border-white ml-4 h-full w-[10%] rounded-l-lg mt-1 mb-1 hover:bg-white hover:text-black">
+         c-
+         </div>
+         <div className="flex border-2 justify-center text-white border-white h-full w-[10%] mt-1 mb-1 hover:bg-white hover:text-black">
+          []
+         </div>
+         <div className="flex border-2 justify-center text-white border-white h-full w-[10%] rounded-r-lg mt-1 mb-1 hover:bg-white hover:text-black">
+          -D
+         </div> 
+
+        </div>
+        <div className="border-2 border-black m-2 h-full w-[99%] rounded-lg bg-white">
+        </div>
+      </div>
+
+
+    </div>
+  );
+}
+
+export default App;
